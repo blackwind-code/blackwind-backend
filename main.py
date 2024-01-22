d15d881c76edaf236e6cd3b5a0b1bb3ef8d9128a
raspberry-pi <raspberry-pi@kakao.com>
2024-01-22 21:05:07 +0900

    Add 401 exception in failed login

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from fastapi.middleware.cors import CORSMiddleware
 
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 
 app = FastAPI()
@@ -17,7 +17,6 @@ app.add_middleware(
 def read_root():
     return "Hello World!"
 
-
 class LoginRequestDto(BaseModel):
     email: str
     password: str
@@ -26,9 +25,8 @@ class LoginResponseDto(BaseModel):
     success: bool
 
 @app.post('/login')
-def login(login_body:LoginRequestDto):
-    
+async def login(login_body:LoginRequestDto):
     if login_body.email == 'raspberry-pi@dgist.ac.kr' and login_body.password == 'qwerty1234':
         return LoginResponseDto(success=True)
     else:
-        return LoginResponseDto(success=False)
+        raise HTTPException(status_code=401, detail="Invalid credentials")
