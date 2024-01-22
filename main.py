5788c4255b9034cd4e3be0f6ad206ce4de086601
raspberry-pi <raspberry-pi@kakao.com>
2024-01-22 21:03:32 +0900

    Add cors middleware to allow *

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
-from typing import Union
+from fastapi.middleware.cors import CORSMiddleware
 
 from fastapi import FastAPI
 from pydantic import BaseModel
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def read_root():
