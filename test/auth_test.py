666ad801b7a6a847b517b479fb6df15dd91cdab0
raspberry-pi <raspberry-pi@kakao.com>
2024-02-12 20:50:53 +0900

    Add the register test

--- test/auth_test.py
+++ test/auth_test.py
@@ -1,12 +1,26 @@
-from infra.database import get_session
+import pytest
+from sqlalchemy import create_engine
+from sqlmodel import SQLModel, Session
 from service.auth_service import AuthService
 
-test_db_engine = create_engine
+test_db_engine = create_engine("sqlite:///test.db")
+SQLModel.metadata.drop_all(test_db_engine, SQLModel.metadata.tables.values())
+SQLModel.metadata.create_all(test_db_engine)
 
-engine = create_engine(DATABASE_CONNECTION_STRING, echo=True)
 
+def get_test_session():
+    with Session(test_db_engine) as session:
+        yield session
 
-def test_AuthService():
-    db_session = get_session()
-    AuthService()
+@pytest.mark.asyncio
+async def test_AuthService():
+    db_session = get_test_session()
+    auth_service = AuthService(next(db_session))
+
+    user = await auth_service.register('psm','psm7177@naver.com', '1234', 1234, 'SUS')
+    assert user.id is not None
+    assert user.username == 'psm'
+    assert user.email == 'psm7177@naver.com'
+    assert user.student_id == 1234
+    assert user.department == 'SUS'
 
