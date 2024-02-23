c0ac9cf3c701da59dffd2c6663ec42fcd7377895
raspberry-pi <raspberry-pi@kakao.com>
2024-02-23 10:21:17 +0900

    Implement auth service test demo

--- env.py
+++ env.py
@@ -1,13 +1,9 @@
 import os
 
-from dotenv import load_dotenv
-
-load_dotenv()
-
 DATABASE_CONNECTION_STRING = os.environ.get('DATABASE_CONNECTION_STRING')
 
 SMTP_SERVER = os.environ.get('SMTP_SERVER')
-MAIL_SYSTME_ADDRESS = os.environ.get('MAIL_SYSTEM_ADDRESS')
+MAIL_SYSTEM_ADDRESS = os.environ.get('MAIL_SYSTEM_ADDRESS')
 MAIL_SYSTEM_USERNAME = os.environ.get('MAIL_SYSTEM_USERNAME')
-MAIO_SYSTEM_PASSWORD = os.environ.get('MAIL_SYSTEM_PASSWORD')
+MAIL_SYSTEM_PASSWORD = os.environ.get('MAIL_SYSTEM_PASSWORD')
 
